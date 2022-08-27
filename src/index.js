1ffc885bfa4080d3dd426bb4164dd6797693333c
jesse sheehan <jessetsheehan@gmail.com>
2022-08-27 13:18:35 -0400

    starter code and starter react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//from stack overflow:
+
+// import React from "react";
+// import { render } from "react-dom";
+// import "./index.css";
+// import App from "./App";
+
+// const root = document.getElementById("root");
+// render(<App />, root);
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
